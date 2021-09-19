e1ef2d0d8cde7a8b67dbdd63d0dd095a86c0d61d
xxxlol1 <diegowall39@gmail.com>
2021-09-19 14:42:37 -0600

    Service and routes done

--- front-end/src/app/service/service.service.ts
+++ front-end/src/app/service/service.service.ts
@@ -1,16 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Time } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
-  url ="/api";
+  url = "/api";
   constructor(private http: HttpClient) { }
 
   //Get meetings
-  getMettings(){
+  getMettings() {
     return this.http.get(this.url);
   }
-  
+
+  //Get meetings by id
+  getMeetingId(id: string) {
+    return this.http.get(this.url + '/' + id);
+  }
+
+  //Deleting Meetings
+  deleteMeeting(id: string) {
+    return this.http.delete(this.url + '/' + id);
+  }
+  //Adding Meetings
+  addMeeting(meeting: Meeting) {
+    return this.http.post(this.url, meeting);
+  }
+
+  //List of available
+  getMeetingAvailable(available: any, finish: any) {
+    return this.http.post(this.url, available, finish);
+  }
+
+
+
+}
+export interface Meeting {
+  meetingId?:string,
+  meetingTime?: Time,
+  availableTime?: Time,
+  userId?: string
 }
